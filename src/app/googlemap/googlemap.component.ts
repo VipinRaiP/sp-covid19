dfe86eedb40fbcd88b9edffa2ef523b84d5f5f86
Harshabh <harshabhmahant11@gmail.com>
2020-05-07 17:22:22 +0530

    added map

--- src/app/googlemap/googlemap.component.ts
+++ src/app/googlemap/googlemap.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import {} from 'googlemaps';
 
 @Component({
   selector: 'app-googlemap',
@@ -7,9 +8,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GooglemapComponent implements OnInit {
 
+  @ViewChild('map',{ static: true }) mapElement: any;
+  public map: google.maps.Map;
   constructor() { }
 
   ngOnInit() {
+    const mapProperties = {
+      center: new google.maps.LatLng(20.5937, 78.9629),
+      zoom: 5,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+ };
+ this.map = new google.maps.Map(this.mapElement.nativeElement,    mapProperties);
+
+ var marker = new google.maps.Marker({
+  position: { lat: 14.581383, lng: 75.634712 },
+  map: this.map,
+  // icon: {
+  //     //url: "https://maps.gstatic.com/intl/en_us/mapfiles/markers2/measle.png",
+  //     url: "http://maps.gstatic.com/mapfiles/markers2/measle_blue.png",
+  //     size: new google.maps.Size(9, 9),
+  //     anchor: new google.maps.Point(3.5, 3.5)
+  // }
+
+});
+
   }
 
 }
