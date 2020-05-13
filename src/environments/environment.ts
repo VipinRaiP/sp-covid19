b4f7f74c6a76b13d676f7747bf034547dc95a89a
Harshabh <harshabhmahant11@gmail.com>
2020-05-13 13:37:04 +0530

    ip to aws

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  //backendIp :"http://3.7.55.162:",
-  backendIp: "http://localhost:",
+  backendIp :"http://13.233.9.170:",
+  // backendIp: "http://localhost:",
   backendPort: 3000
 };
 
