426a4a59cc11a520b657de6bdca323cbafb49a44
VipinRaiP <vipin.rai.p@gmail.com>
2020-05-20 16:26:56 +0530

    Added merge region feature

--- src/app/show-map/show-map.component.ts
+++ src/app/show-map/show-map.component.ts
@@ -18,9 +18,9 @@ export class ShowMapComponent implements OnInit {
   // public value: Date = new Date(2020, 5, 1, 22);
   public format: string = 'dd/MM/yyyy HH:mm';
 
-  public fromDate: Date = new Date(2020, 5, 1, 22);
-  public toDate: Date = new Date(2020, 6, 1, 22);
-
+  public fromDate: Date = new Date(2020, 0, 1, 22);
+  //public toDate: Date = new Date(2020, 6, 1, 22);
+  public toDate: Date = new Date();
 
   constructor(public mapService:MapService) { }
 
@@ -49,9 +49,16 @@ export class ShowMapComponent implements OnInit {
 
   mergeData(){
     console.log("Merge Data");
+    let ipAddress = [];
+    document.getElementsByName("region-ip").forEach((d) => {
+      ipAddress.push((<HTMLInputElement>d).value);
+    });
+    console.log("IP Address");
+    console.log(ipAddress[0]);
     let data  = {
       startdate : this.fromDate,
-      enddate : this.toDate
+      enddate : this.toDate,
+      ipAddress : ipAddress[0]
     }
     this.mapService.mergeDataService.emit(data);
   }
